2f8cf1f338afe062a381ad32ea8636585e747024
admin <admin@YD01-13>
2024-07-03 13:53:25 +0900

    상품상세 찜 작성

--- strongRefresh/src/main/java/co/sf/heart/service/HeartService.java
+++ strongRefresh/src/main/java/co/sf/heart/service/HeartService.java
@@ -2,6 +2,7 @@ package co.sf.heart.service;
 
 import java.util.List;
 
+import co.sf.heart.vo.HeartVO;
 import co.sf.product.vo.ProductVO;
 
 public interface HeartService {
@@ -10,5 +11,14 @@ public interface HeartService {
 	
 	// 삭제
 	boolean removeHeart(String pcode);
+	
+	// 상품 -> 찜 확인
+	int getPrdHeart(HeartVO hvo);
+	
+	// 상품 -> 찜 삭제
+	boolean remPrdHeart(HeartVO hvo);
+	
+	// 상품 -> 찜 추가
+	boolean productToHeart(HeartVO hvo);
 
 }
