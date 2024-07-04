c61e70f597e24cde7ccce284017e670e5de990bb
yujin <yujin@192.168.10.39>
2024-07-04 09:05:44 +0900

    2024-07-04

--- strongRefresh/src/main/java/co/sf/user/web/MyPageForm.java
+++ strongRefresh/src/main/java/co/sf/user/web/MyPageForm.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import co.sf.common.Control;
 
@@ -13,6 +14,12 @@ public class MyPageForm implements Control {
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//세션은 object니까 형변환해줘야함
+		//로그인 시 생성한 세션 똑같이 사용가능
+		HttpSession session = req.getSession();
+		String id = (String)session.getAttribute("id");
+		System.out.println(id);
+		
 		req.getRequestDispatcher("cyj/mypageForm.tiles").forward(req, resp);
 	}
 
